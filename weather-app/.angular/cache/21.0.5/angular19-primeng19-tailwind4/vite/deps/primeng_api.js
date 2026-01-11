2d8ecd6202233e93bdb9392e70e183be825518be
Menko <menko@hornstein.me>
2026-01-11 21:53:22 +0100

    basic functions

---
+++ weather-app/.angular/cache/21.0.5/angular19-primeng19-tailwind4/vite/deps/primeng_api.js
@@ -0,0 +1,37 @@
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-L4GVSIS7.js";
+import "./chunk-IWUS5Z4G.js";
+import "./chunk-VA3RDU3M.js";
+import "./chunk-H2SRQSE4.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
---
+++ weather-app/src/app/app.config.ts
@@ -0,0 +1,25 @@
+console.log('APP CONFIG LOADED');
+
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura  from '@primeng/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark-mode'
+        }
+      }
+    })
+  ]
+};
